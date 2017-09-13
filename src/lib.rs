81b3e17b1ba9453f99f28c9e8a2bb6aae7e852b8
Alberto Leal <mailforalberto@gmail.com>
2017-09-13 03:29:21 -0400

    init traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+
+trait DataPoint<T> {
+    fn open() -> T;
+    fn high() -> T;
+    fn low() -> T;
+    fn close() -> T;
+
+    fn volume() -> T;
+}
+
+trait Chart<T> {
+
+    type Item: DataPoint<T>;
+
+    fn get(back_index: u64) -> Self::Item;
+
+
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
