0f57db1524379be4d12a916fa8b33b4cc898988c
Alberto Leal <mailforalberto@gmail.com>
2017-09-16 02:37:40 -0400

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,66 @@
+// traits
 
-trait DataPoint<T> {
-    fn open() -> T;
-    fn high() -> T;
-    fn low() -> T;
-    fn close() -> T;
+struct DataPoint<T> {
+    open: T,
+    high: T,
+    low: T,
+    close: T,
 
-    fn volume() -> T;
+    volume: T,
 }
 
 trait Chart<T> {
-
-    type Item: DataPoint<T>;
-
-    fn get(back_index: u64) -> Self::Item;
-
-
+    fn get(&self, index: u64) -> DataPoint<T>;
 }
+
+// chart styles
+
+struct Candlestick;
+
+struct Renko;
+
+// indicators
+
+// ichimoku
+
+struct Ichimoku<T> {
+    chart: Box<Chart<T>>,
+}
+
+impl<T> Ichimoku<T> {
+    fn new(
+        chart: Box<Chart<T>>,
+
+        // params
+        conversion_line_period: u64,
+        base_line_period: u64,
+        span_b_period: u64,
+        lagging_span_displacement: u64,
+    ) -> Self {
+        Ichimoku { chart: chart }
+    }
+
+    fn turning_line(index: u64) -> f64 {
+        0.0
+    }
+
+    fn standard_line(index: u64) -> f64 {
+        0.0
+    }
+
+    fn span_a(index: u64) -> f64 {
+        0.0
+    }
+
+    fn span_b(index: u64) -> f64 {
+        0.0
+    }
+
+    fn lagging_line(index: u64) -> f64 {
+        0.0
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
