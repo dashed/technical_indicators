281f0d0b0ec2be66906c22620106d227227bcdd4
Alberto Leal <mailforalberto@gmail.com>
2017-12-24 04:27:34 -0500

    wma

--- src/indicators/mod.rs
+++ src/indicators/mod.rs
@@ -4,5 +4,8 @@ pub use self::sma::SimpleMovingAverage;
 mod ema;
 pub use self::ema::ExponentialMovingAverage;
 
+mod wma;
+pub use self::wma::WeightedMovingAverage;
+
 mod ichimoku;
 pub use self::ichimoku::Ichimoku;
---
+++ src/indicators/wma.rs
@@ -0,0 +1,48 @@
+use charts::SourceSeries;
+
+// Exponential Moving Average (EMA)
+
+pub struct WeightedMovingAverage<'source> {
+    source: SourceSeries<'source>,
+
+    // params
+    length: usize,
+}
+
+impl<'source> WeightedMovingAverage<'source> {
+    pub fn new(source: SourceSeries<'source>, length: usize) -> Self {
+
+        WeightedMovingAverage {
+            source: source,
+            length: length,
+        }
+    }
+
+    pub fn get(&self, index: usize) -> Option<f64> {
+
+        if self.length <= 0 {
+            return None;
+        }
+
+        let offset_source = self.source.offset(index);
+
+        let mut norm = 0.0;
+        let mut total = 0.0;
+
+        for normalized_index  in 0..self.length {
+            match offset_source.get(normalized_index) {
+                None => {
+                    return None;
+                }
+                Some(data) => {
+                    let weight = (self.length - normalized_index) * self.length;
+                    let weight = weight as f64;
+                    norm = norm + weight;
+                    total = total + data * weight;
+                }
+            }
+        }
+
+        Some(total / norm)
+    }
+}
